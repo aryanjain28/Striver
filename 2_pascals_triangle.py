3ac768edd7724ebaaa909596cf731fdba04d04bc
Aryan Jain <aryanjn807@gmail.com>
2023-01-06 09:47:42 -0500

    -> added 2 more questions for pascals triangle

--- 2_pascals_triangle.py
+++ 2_pascals_triangle.py
@@ -1,3 +1,13 @@
+"""
+3 Questions related to Pascal's
+
+1. Get Pascal's Triangle
+2. Get Element at Row,Col
+3. Get Row at N
+
+"""
+
+# 1. To get complete Triangle
 def pascals_triangle(n):
     list = []
     if n == 1: 
@@ -19,6 +29,31 @@ def pascals_triangle(n):
     return list
 
 triangle = pascals_triangle(6)
-print(triangle)
+# print(triangle)
+
+def get_factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    return n * get_factorial(n-1)
+
+# 2. Get Element at (Row,Col)
+def get_element_at_position(row, col):
+    # Use (row-1)C(col-1) to get the element
+
+    fact_row = get_factorial(row - 1)
+    fact_col = get_factorial(col - 1) * get_factorial(row - col)
+
+    return fact_row // fact_col
+
+# print(get_element_at_position(5, 3))
+
+# 3. Get complete row
+def get_row(n):
+    row = []
+    for i in range(1, n+1):
+        row.append(get_element_at_position(n, i))
+    return row
+
+print(get_row(20))
 
 
