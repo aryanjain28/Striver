a241becd5bab0dc59457e63224fdceec9d8bc2ba
Aryan Jain <aryanjn807@gmail.com>
2023-01-19 07:37:05 -0500

    -> fixed 3_2 according to leet

--- 3_2_Implement_Pow.py
+++ 3_2_Implement_Pow.py
@@ -1,5 +1,11 @@
 def implement_pow_binary(x, n):
     extra = 1
+    if n == 0: return 1
+    
+    if n < 0:
+        x = 1 / x
+        n = -1 * n
+        
     while n > 1:
         if n % 2 != 0:
             n = n - 1
@@ -10,5 +16,5 @@ def implement_pow_binary(x, n):
 
     return x * extra
 
-output = implement_pow_binary(3.0000, 4)
+output = implement_pow_binary(2.00000, -2)
 print(output)
