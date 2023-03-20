31d746873e231cea10163471d17826a6d67c2a55
Aryan Jain <aryanjn807@gmail.com>
2023-03-20 11:13:45 -0400

    -> update 1838

--- medium_leetCode/1838.frequency-of-the-most-frequent-element.py
+++ medium_leetCode/1838.frequency-of-the-most-frequent-element.py
@@ -15,12 +15,28 @@ class Solution(object):
 
         nums.sort()
         i = 0
-        for j in range(len(nums)):
-            k += nums[j]
-            if k < nums[j] * (j - i + 1):
-                k -= nums[i]
+        j = 0
+        total = 0
+        windowLen = -1
+
+        while j < len(nums):
+            total += nums[j]
+            while k + total < nums[j] * (j - i + 1):
+                total -= nums[i]
                 i += 1
-        return j - i + 1
+
+            windowLen = max(windowLen, j - i + 1)
+            j += 1
+
+        return windowLen
+
+        # for j in range(len(nums)):
+        #     k += nums[j]
+        #     if k < nums[j] * (j - i + 1):
+        #         k -= nums[i]
+        #         i += 1
+        # return j - i + 1
 
 
 # @lc code=end
+Solution().maxFrequency([1, 2, 4], 5)
