414850fcd49eb645cf11c1760ba7f50c4a3b576b
Aryan Jain <aryanjn807@gmail.com>
2023-03-23 09:02:00 -0400

    -> 735 no impl

--- 735.asteroid-collision.py
+++ 735.asteroid-collision.py
@@ -8,20 +8,9 @@
 class Solution:
     def asteroidCollision(self, asteroids):
 
-        res = []
-        for _ in range(len(asteroids) - 1):
-            d1 = asteroids.pop()
-            d2 = asteroids.pop()
+        mStack = []
 
-            if d1 < 0 and d2 < 0:
-                return
-            elif d1 > 0 and d2 > 0:
-                return
-
-            if abs(d1) > abs(d2):
-                asteroids.append(d1)
-            elif abs(d1) < abs(d2):
-                asteroids.append(d2)
+        
 
         return asteroids
 
