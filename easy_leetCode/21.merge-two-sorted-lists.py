a433ffc4a26204ff3f9473e694670aea702fbdf8
Aryan Jain <aryanjn807@gmail.com>
2023-04-01 10:19:07 -0400

    -> added more on 21.

--- easy_leetCode/21.merge-two-sorted-lists.py
+++ easy_leetCode/21.merge-two-sorted-lists.py
@@ -6,10 +6,12 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
         """
@@ -18,38 +20,62 @@ class Solution(object):
         :rtype: Optional[ListNode]
         """
 
-        if list1 == None:
-            return list2
+        # New list
+        # if list1 == None and list2 == None:
+        #     return list1
 
-        elif list2 == None:
-            return list1
+        # head = new = ListNode()
+        # while list1 and list2:
+        #     if list1.val < list2.val:
+        #         new.val = list1.val
+        #         list1 = list1.next
+        #     else:
+        #         new.val = list2.val
+        #         list2 = list2.next
 
-        elif list1 == None and list2 == None:
-            return list1
+        #     new.next = ListNode()
+        #     new = new.next
 
-        head = None
-        if list1.val <= list2.val:
-            curr = list1
-            other = list2
-        else:
-            curr = list2
-            other = list1
+        # list = list1 if list1 != None else list2
 
-        head = curr
-        while curr.next and other:
+        # new.val = list.val
+        # new.next = list.next
 
-            if curr.next.val > other.val:
-                # break and join other
-                next = curr.next
-                curr.next = other
-                curr = other
-                other = next
+        # return head
 
-            else:
-                curr = curr.next
+        # In place
+        # if list1 == None:
+        #     return list2
 
-        curr.next = other
-        return head
+        # elif list2 == None:
+        #     return list1
+
+        # elif list1 == None and list2 == None:
+        #     return list1
+
+        # head = None
+        # if list1.val <= list2.val:
+        #     curr = list1
+        #     other = list2
+        # else:
+        #     curr = list2
+        #     other = list1
+
+        # head = curr
+        # while curr.next and other:
+
+        #     if curr.next.val > other.val:
+        #         # break and join other
+        #         next = curr.next
+        #         curr.next = other
+        #         curr = other
+        #         other = next
+
+        #     else:
+        #         curr = curr.next
+
+        # curr.next = other
+        # return head
 
 
 # @lc code=end
