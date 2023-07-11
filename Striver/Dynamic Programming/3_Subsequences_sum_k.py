d75dd0d0fe621a98c789b2435e591bc2bc3e58b8
Aryan Jain <aryanjn807@gmail.com>
2023-07-11 17:18:36 -0400

    update 3_Subsequences_sum_k.py

--- Striver/Dynamic Programming/3_Subsequences_sum_k.py
+++ Striver/Dynamic Programming/3_Subsequences_sum_k.py
@@ -7,21 +7,14 @@ def subseq_k(nums, k):
     def rec(index, arr, curr):
 
         if curr == k:
-            print(arr)
-            return True
+            return 1
 
         if index >= n:
-            return False
+            return 0
 
-        if rec(index+1, arr + [nums[index]], curr + nums[index]):
-            return True
-
-        if rec(index+1, arr, curr):
-            return True
-
-        return False
+        return rec(index+1, arr + [nums[index]], curr + nums[index]) + rec(index+1, arr, curr)
 
     return rec(0, [], 0)
 
 
-subseq_k([1, 2, 1], 2)
+print(subseq_k([1, 2, 1], 2))
