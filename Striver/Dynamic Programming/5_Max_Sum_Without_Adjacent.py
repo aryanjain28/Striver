1aa18da9143b3f0af74f5f0a2aeb4d8d87e14bc7
Aryan Jain <aryanjn807@gmail.com>
2023-07-12 17:59:20 -0400

    update 5_Max_Sum_Without_Adjacent.py

--- Striver/Dynamic Programming/5_Max_Sum_Without_Adjacent.py
+++ Striver/Dynamic Programming/5_Max_Sum_Without_Adjacent.py
@@ -1,39 +1,51 @@
 def maximumNonAdjacentSum(nums):
 
-    n = len(nums)
+    dp = {}
 
-    # dp = {}
+    def rec(i):
 
-    # def rec(i):
+        if i in dp:
+            return dp[i]
 
-    #     if i == 0:
-    #         return nums[0]
+        if i == 0:
+            return nums[0]
 
-    #     if i in dp:
-    #         return dp[i]
+        if i <= 0:
+            return 0
 
-    #     maximum = -1
+        pick = nums[i] + rec(i-2)
+        nPick = rec(i-1)
+        dp[i] = max(pick, nPick)
+
+        return dp[i]
+
+    return rec(len(nums)-1)
+
+    # dp[i] = maximum
+    # return dp[i]
+
+    # res = []
+    # for m in range(n-1, -1, -1):
+    #     res.append(rec(m))
+
+    # return max(res)
+
+    # n = len(nums)
+    # dp = [nums[0]] * n
+
+    # for i in range(1, n):
+    #     print(dp, nums[i])
+    #     maximum = nums[i]
     #     for j in range(2, i+1):
-    #         maximum = max(maximum, rec(i-j) + nums[i])
+    #         if i >= j:
+    #             maximum = max(maximum, dp[i-j] + nums[i])
 
-    #     dp[i] = maximum
-    #     return dp[i]
+    #     dp[i] = max(dp[i-1], maximum)
 
-    # return rec(n-1)
-
-    dp = [0] * n
-    dp[0] = nums[0]
-
-    for i in range(1, n):
-        maximum = -1
-        for j in range(2, i+1):
-            maximum = max(maximum, dp[i-j] + nums[i])
-
-        dp[i] = maximum
-
-    return dp[-1]
-
-
-print(maximumNonAdjacentSum([5, 5, 10, 100, 10, 5]))
-print(maximumNonAdjacentSum([3, 2, 5, 10, 7]))
-print(maximumNonAdjacentSum([3, 2, 7, 10]))
+    # print(dp)
+    # return dp[-1]
+# print(maximumNonAdjacentSum([5, 5, 10, 100, 10, 5]))
+# print(maximumNonAdjacentSum([3, 2, 5, 10, 7]))
+# print(maximumNonAdjacentSum([3, 2, 7, 10]))
+# print(maximumNonAdjacentSum([9, 9, 8, 2]))
+print(maximumNonAdjacentSum([3, 10, 10, 7]))
